69e3d9de2bf008c0067a781d5038926b30113708
Dhimas Pandu Yogaswara <79622243+Dhimasswara@users.noreply.github.com>
2023-06-11 12:26:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 }))
 app.use(
     cors({
-      origin: ['https://letsbuy-ui.vercel.app', 'http://localhost:3000'],a
+      origin: ['https://letsbuy-ui.vercel.app', 'http://localhost:3000'],
       credentials: true,
       methods: "GET, POST, PUT, DELETE",
     })
